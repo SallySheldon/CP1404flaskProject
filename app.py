9f871c0502b96e9347a87ec2db7c16bc0780ba7a
SallySheldon <sally.sheldon@my.jcu.edu.au>
2023-08-10 22:55:08 +1000

    Complete Flask project

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    """Convert degrees celsius to degrees fahrenheit."""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return f"{celsius} degrees Celsius = {fahrenheit} degrees Fahrenheit"
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def f(celsius=float()):
+    return str(convert_celsius_to_fahrenheit(float(celsius)))
+
+
+if __name__ == '__main__':
+    app.run()
